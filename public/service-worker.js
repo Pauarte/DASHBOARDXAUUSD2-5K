52c30c77749d5b39de8d11777f1dd1ff7edc83dc
Gartecz <oriolceres@gmail.com>
2026-07-28 23:18:34 +0200

    Brand PWA as Monitor Bots Trading

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE = 'xauusd-dashboard-v1'
+const CACHE = 'monitor-bots-v2'
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['/','/manifest.webmanifest','/icon.svg'])))
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['/','/manifest.webmanifest','/icon.png'])))
 })
 self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim())
